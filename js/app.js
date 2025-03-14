7a2c0177f357991bf6f765fd77c8da472c94f6d6
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-14 17:55:50 +0100

    feat: all slides added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 const gridOutput = document.getElementById('grid');
-gridOutput.innerHTML = ''
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
         .then(element => {
             console.log(element.data);
-            gridOutput.innerHTML += `<div>
-                                        <img id="pin" src="img/pin.svg" alt="pin">
-                                        <img src=${element.data[0].url} alt="">
-                                        <p>${element.data[0].title}</p>
-                                     </div>`
+            console.log('prima del for');
+            for (i = 0; i < element.data.length; i++) {
+                gridOutput.innerHTML += `<div>
+                                            <img id="pin" src="img/pin.svg" alt="pin">
+                                            <img src=${element.data[i].url} alt="">
+                                            <p>${element.data[i].title}</p>
+                                        </div>`;
+                console.log('elemento aggiunto');
+            };
+            console.log('dopo il for');
         })
+        .catch(error => {
+            console.error('Errore nella richiesta:', error);
+            gridOutput.innerHTML = `<li>errore nel caricamento dei dati</li>`;
+        });
