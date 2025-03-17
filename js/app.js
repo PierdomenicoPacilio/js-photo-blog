6dd8760773842539674371bbe559474f5de16202
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-17 16:50:06 +0100

    feat: second milestone

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 const gridOutput = document.getElementById('grid');
+const cards = document.getElementsByClassName('col');
+const overlay = document.getElementById('overlay');
+const button = document.getElementById('button');
+
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
         .then(element => {
             console.log(element.data);
             console.log('prima del for');
             for (i = 0; i < element.data.length; i++) {
-                gridOutput.innerHTML += `<div>
+                gridOutput.innerHTML += `<div class="col">
                                             <img id="pin" src="img/pin.svg" alt="pin">
                                             <img src=${element.data[i].url} alt="">
                                             <p class="data">${element.data[i].date}</p>
@@ -13,6 +17,17 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
                 console.log('elemento aggiunto');
             };
             console.log('dopo il for');
+            for (i = 0; i < cards.length; i++) {
+                cards[i].addEventListener('click', function() {
+                    overlay.classList.add('container');
+                    overlay.classList.remove('none');
+                });
+                console.log('evento aggiunto');
+            };
+            button.addEventListener('click', function() {
+                overlay.classList.add('none');
+                overlay.classList.remove('container');     
+            });
         })
         .catch(error => {
             console.error('Errore nella richiesta:', error);
