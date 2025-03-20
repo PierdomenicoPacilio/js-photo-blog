8f509fbcc706b47b7dc02aea7a729180f4a3b428
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-20 14:48:47 +0100

    docs: comment

--- js/app.js
+++ js/app.js
@@ -1,16 +1,38 @@
-const body = document.getElementById('body');
+//  DICHIARAZIONE DELLE VARIABILI
+
+const body = document.body;
 const gridOutput = document.getElementById('grid');
-const cards = document.getElementsByClassName('col');
 const overlay = document.getElementById('overlay');
 
 
+
+//  UTILIZZO DI AXIOS PER ACCEDERE ALL'API
+
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
+
+
+        //  UTILIZZO DI THEN PER GESTIRE INFORMAZIONI E CONTENUTI DELL'API
+
         .then(element => {
+
+            // controllo che sia l'array di oggetti che mi interessa
             console.log(element.data);
+
             overlay.innerText = '';
+
+            // verifico funionamento dell'iterazione
             console.log('prima del for');
+
+
+            // inizio l'iterazione che mi porta a creare il layout principale della pagina tramite gli elementi presi dall'API
+
             for (i = 0; i < element.data.length; i++) {
+
                 const currentElement = element.data[i];
+
+
+                // aggiungo gli elementi alla griglia
+
                 // gridOutput.innerHTML += `<div id="col${[i]}">
                 //                             <img id="pin" src="img/pin.svg" alt="pin">
                 //                             <img src=${currentElement.url} alt="">
@@ -23,8 +45,13 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
                                                                 <p class="data">${currentElement.date}</p>
                                                                 <p class="title">${currentElement.title.toUpperCase()}</p>
                                                             </div>`);
+
+
+                // prendo ora un elemento alla volta
                 const currentHTMLElement = document.getElementById(`col${[i]}`);
                 console.log(currentHTMLElement);
+
+                // funzione che permette l'utilizzo dell'overlay
                 const overlayFunction = function(url) {
                     overlay.classList.add('container');
                     overlay.classList.remove('none');
@@ -38,37 +65,23 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
                         overlay.innerHTML = '';
                     });
                 };
+
+                // aggiungo l'evento all'elemento appena aggiunto
                 currentHTMLElement.addEventListener('click', () => overlayFunction(currentElement.url) );
+
+                // verifico funionamento dell'iterazione
                 console.log('elemento aggiunto');
             };
-            console.log('dopo il for');
-            // for (i = 0; i < cards.length; i++) {
-            //     cards[i].addEventListener('click', function() {
-            //         overlay.classList.add('container');
-            //         overlay.classList.remove('none');
-            //         body.classList.add('ov-hid');
-            //     });
-            //     console.log('evento aggiunto');
-            // };
 
+            // verifico funionamento dell'iterazione
+            console.log('dopo il for');
         })
+
+
+
+        //   UTILIZZO DI CATCH PER GESTIRE EVENTUALI ERRORI
+
         .catch(error => {
             console.error('Errore nella richiesta:', error);
-            gridOutput.innerHTML = `<li>errore nel caricamento dei dati</li>`;
+            gridOutput.innerHTML = `<div>errore nel caricamento dei dati</div>`;
         });
-
-
-
-        // function() {
-        //     overlay.classList.add('container');
-        //     overlay.classList.remove('none');
-        //     body.classList.add('ov-hid');
-        //     overlay.innerHTML = `<button id="button">Chiudi</button><img src="${currentElement.url}" alt="">`;
-        //     const button = document.getElementById('button');
-        //     button.addEventListener('click', function() {
-        //         overlay.classList.add('none');
-        //         overlay.classList.remove('container');   
-        //         body.classList.remove('ov-hid');
-        //         overlay.innerHTML = '';
-        //     });
-        // }
